a26030df1118a082d989bdf1d7d024bf42bece50
adamgdm <116694569+adamgdm@users.noreply.github.com>
2023-01-10 10:01:45 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -2,20 +2,21 @@
 #define GET_NEXT_LINE
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 1000
 #endif
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-char	*ft_rm(char *str);
-char	*ft_return(char *str);
-char	*ft_read(char *str, int fd, int line);
-char	*ft_restr(char *str, int fd);
 char	*get_next_line(int fd);
-char    *ft_substr(char const *s, int start, int len);
-char	*ft_strjoin(char const *s1, char const *s2);
-int		ft_strlen(const char *s);
+char	*ft_read(int fd, char *str);
+char	*ft_return(char *str, int i, int j);
+char	*ft_rm(char *str, int i, int j);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2, int i, int j);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 
 #endif
