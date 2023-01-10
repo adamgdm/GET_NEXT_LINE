92bbed3ec4070731281cd3e680f5b14974f7f165
adamgdm <116694569+adamgdm@users.noreply.github.com>
2023-01-10 09:53:55 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,63 +1,91 @@
 #include "get_next_line.h"
 
-int	ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+typedef struct t_lens 
 {
-	int		total;
-	char	*str;
-	int		i;
-	int		j;
-	int		k;
+	int len_s1;
+	int len_s2;
+	int total;
+}	t_len;
 
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
-		return (0);
-	k = ft_strlen(s1);
-	total = ft_strlen(s1) + ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (total + 1));
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	int		lsrc;
+	char	*str;
+
+	str = (char *)s;
 	if (!str)
 		return (0);
-	while (i < total)
+	i = 0;
+	lsrc = ft_strlen(s);
+	if ((unsigned char)c == '\0')
+		return (str + lsrc);
+	while (i <= lsrc && str[i])
 	{
-		while (i < k)
-			str[i++] = s1[j++];
-		j = 0;
-		while (i < total)
-			str[i++] = s2[j++];
+		if (str[i] == (unsigned char)c)
+			return (str + i);
+		i++;
 	}
-	str[i] = '\0';
-	return (str);
+	if (!c)
+		return (str + i);
+	return (0);
 }
 
-char	*ft_substr(char const *s, int start, int len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int		i;
-	int		sub_len;
+	size_t	i;
+	size_t	sub_len;
 	char	*substr;
 
 	i = 0;
 	if (!s)
 		return (0);
 	sub_len = start;
-	if (len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	if (start > ft_strlen(s))
-		len = 0;
-	substr = (char *)malloc(sizeof(char) * (len + 1));
+	substr = calloc(sizeof(char), (len + 1));
 	if (!substr || !s)
 		return (0);
 	while (start < sub_len + len)
 		substr[i++] = s[start++];
 	substr[i] = '\0';
 	return (substr);
+}
+
+char	*ft_strjoin(char *s1, char *s2, int i, int j)
+{
+	char	*new;
+	t_len		x;
+
+	if (!s2)
+		return (NULL);
+	if (!s1)
+		return (ft_substr(s2, 0, ft_strlen(s2)));
+	i = 0;
+	j = ft_strlen(s1);
+	x.len_s1 = ft_strlen(s1);
+	x.len_s2 = ft_strlen(s2);
+	new = malloc((x.len_s1 + x.len_s2 + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	while (i < x.len_s1)
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < x.len_s2)
+		new[j++] = s2[i++];
+	new[j] = '\0';
+	return (new);
 }
